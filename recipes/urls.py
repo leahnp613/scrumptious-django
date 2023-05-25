c3f260dee7cc24f40bd08fb908239718e7f4ad30
leahnp613 <leahnagarpowers@gmail.com>
2023-05-25 12:34:35 -0400

    combined html and css

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from recipes.views import show_recipe
+from recipes.views import recipe_list, show_recipe
 
 
 urlpatterns = [
-    path("recipes/<int:id>/", show_recipe),
+    path("recipes/<int:id>/", show_recipe),\
+    path("recipes/<int:id>", recipe_list)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -8,3 +8,11 @@ def show_recipe(request, id):
         "recipe_object" :recipe,
     }
     return render(request, "recipes/detail.html", context)
+
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    context = {
+        "recipe_list":recipes,
+    }
+    return redner(request, "recipes/list.html", context)
