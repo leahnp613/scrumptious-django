fc402327295befc3d7d01ce7fa3dc796c328495d
leahnp613 <leahnagarpowers@gmail.com>
2023-05-30 18:50:42 -0400

    fixed template error, added error messsage for passwords do not match

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from recipes.views import recipe_list, show_recipe, create_recipe, edit_recipe
 
-
+app_name = "recipes"
 urlpatterns = [
     path("recipes/<int:id>/", show_recipe, name="show_recipe"),
     path("recipes/", recipe_list, name="list_recipe"),
