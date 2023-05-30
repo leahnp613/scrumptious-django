907728e6eba76fde6d7e933247a3dbe783bf1095
leahnp613 <leahnagarpowers@gmail.com>
2023-05-30 14:14:37 -0400

    added accouints app

--- scrumptious/settings.py
+++ scrumptious/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "recipes.apps.RecipesConfig"
+    "recipes.apps.RecipesConfig",
+    "accounts.apps.AccountsConfig"
 ]
 
 MIDDLEWARE = [
--- scrumptious/urls.py
+++ scrumptious/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect 
+
+def redirect_to_recipe_list (request):
+    return redirect("recipe_list")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("recipes.urls")),
+   
 ]
