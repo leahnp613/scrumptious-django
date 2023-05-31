b7d5cfc77df8f7ab39496d3e45d04402d16e65a6
leahnp613 <leahnagarpowers@gmail.com>
2023-05-31 08:22:27 -0400

    added loginform

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,3 +7,7 @@ class SignUpForm(forms.Form):
     last_name = forms.CharField(max_length=150)
     password = forms.CharField(max_length=150, widget=forms.PasswordInput)
     password_confirmation = forms.CharField(max_length=150, widget=forms.PasswordInput)
+
+class LogInForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=150, widget=forms.PasswordInput)
