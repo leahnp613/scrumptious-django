30ab9c90df70cf5d6753a412be50ab75f2a20d60
leahnp613 <leahnagarpowers@gmail.com>
2023-05-31 10:22:28 -0400

    stopping point for the day

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from accounts.views import signup
+from accounts.views import signup, user_login
 
 app_name = "accounts"
 urlpatterns = [
     path("signup/", signup, name = "signup"),
+    path("login/" user_login, name = "user_login"),
 ]
