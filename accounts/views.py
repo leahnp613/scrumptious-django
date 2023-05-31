ce5c0626ce39d03023ed5115356e92a933c3e3e8
leahnp613 <leahnagarpowers@gmail.com>
2023-05-31 08:31:49 -0400

    added login view function

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 
 from django.shortcuts import render, redirect 
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
-from accounts.forms import SignUpForm
+from accounts.forms import LogInForm, SignUpForm
 
 # Create your views here.
 def signup(request):
@@ -33,3 +33,27 @@ def signup(request):
         "form": form
     }
     return render(request, "accounts/signup.html", context)
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(
+                request, 
+                username = username,
+                password = password,
+            )
+
+            if user is not None:
+                login(request, user)
+                return redirect('recipes:list_recipe')
+        else:
+            form = LogInForm()
+        context = {
+            "form" : form,
+        }
+        return render(request,"accounts/login.html", context)
